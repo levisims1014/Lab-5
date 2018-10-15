b45cb3ff43789ed40beae5b0f459b3b15bdfff92
levisims1014 <42224339+levisims1014@users.noreply.github.com>
2018-10-15 00:36:22 -0400

    Add files via upload

---
+++ Lab 5/Lab 5/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string user;
+            do
+            {
+                int fact;
+                long x = 1;
+                Console.WriteLine("Welcome to the Factorial Calculator!");
+                Console.WriteLine("Enter an integer that's greater than 0 but less than 10:");
+
+                fact = Convert.ToInt32(Console.ReadLine());
+
+                for (int i = 1; i <= fact; i++)
+                {
+                    x = x * i;
+                    {
+                        Console.WriteLine("Your Factorial = ");
+                        Console.WriteLine(x);
+                    }
+                }
+                Console.WriteLine("Continue? y/n");
+                user = Console.ReadLine().ToLower();
+            }
+            while
+            (user == "y" || user == "yes");
+            Console.WriteLine("Goodbye!");
+        }
+    }
+}
